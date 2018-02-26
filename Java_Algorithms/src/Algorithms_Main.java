5259e12fec73dfe0060f71975a2ec37d0e37b2e0
Shivakumar Mahakali <mahakalim@yahoo.com>
2018-02-25 19:08:32 -0500

    Algorithm_Main.java testing.

---
+++ Java_Algorithms/src/Algorithms_Main.java
@@ -0,0 +1,15 @@
+public class Algorithms_Main {
+
+    public static void main(String[] args) {
+        String myVar = "Hello World!";
+        System.out.println(myVar);
+    }
+
+    // O(1) Algorithm
+    public static int O_of_1() {
+        System.out.println();
+        return 5;
+    }
+
+
+}
